dfe55e073328b6d00246c5867ae919ad95abc0fd
Yoeny Caballero Gonzalez <ycaballero12315@gmail.com>
2025-12-16 08:39:23 -0300

    retos navidennos

---
+++ retos.py
@@ -0,0 +1,89 @@
+def manufacture_gifts(gifts_to_produce: list[dict[str, int]]) -> list[str]:
+    toys: list[str] = []
+    for elm in gifts_to_produce:
+        toy, quantity = elm.get("toy"), elm.get("quantity")
+        if not isinstance(quantity, int) or quantity <=0:
+            continue
+        toys.extend([toy]*quantity)
+
+    return toys
+
+from datetime import datetime
+import math
+
+def time_until_take_off(from_time:str, take_off_time:str)->int:
+    from_timestamp = time_cleaning_elf(from_time)
+    take_off_timestamp = time_cleaning_elf(take_off_time)
+    return take_off_timestamp - from_timestamp
+
+def time_cleaning_elf(time_raw:str):
+    time_not_np = time_raw.replace(" NP", "")
+    date_day, dtae_time = time_not_np.split("@")
+    year, month, day = map(int,date_day.split("*"))
+    hour, minut, secound = map(int,dtae_time.split("|"))
+    dt = datetime(year,month,day,hour,minut,secound)
+    return math.floor(dt.timestamp())
+
+def match_gloves(gloves: list[dict[str, str]]) -> list[str]:
+    left_glove:dict[str,int] = {}
+    right_glove:dict[str,int] = {}
+    pair: list[dict[str, str]]= []
+    for glove in gloves:
+        hand = glove['hand']
+        color = glove['color']
+    
+        if hand == "L":
+            if color in right_glove and right_glove.get(color)>0:
+                pair.append(color)
+                right_glove[color] -=1
+            else:
+                left_glove[color] = left_glove.get(color, 0) + 1
+        elif hand == "R":
+            if color in left_glove and left_glove.get(color)>0:
+                pair.append(color)
+                left_glove[color] -=1
+            else:
+                right_glove[color] = right_glove.get(color, 0) + 1
+
+    return pair
+
+def draw_tree(height:int, 
+              ornament:str, 
+              frequency:int)->str:
+    elem:list[str] = []
+    posicion:int = 1
+    for i in range(height):
+        fila = " "*(height-i-1)
+
+        for j in range(2*i+1):
+            if posicion % frequency == 0:
+                fila += ornament
+            else:
+                fila +="*"
+            posicion += 1
+        elem.append(fila)
+
+    trunk = " " * (height - 1) + "#"
+    elem.append(trunk)
+    
+    return "\n".join(elem)
+    
+if __name__ == "__main__":
+    toys_for_qa = [{'toy': "doll", 'quantity': 3},
+                   {'toy': "robot", 'quantity': 5},
+                   {'toy': "dron", 'quantity': 0}]
+    
+    print(manufacture_gifts(toys_for_qa))
+
+    takeoff = '2025*12*25@00|00|00 NP'
+    print('30 segundos antes:', time_until_take_off('2025*12*24@23|59|30 NP', takeoff))
+
+    gloves = [
+                {'hand': 'L', 'color': 'red'},
+                {'hand': 'R', 'color': 'red'},
+                {'hand': 'R', 'color': 'green'},
+                {'hand': 'L', 'color': 'blue'},
+                {'hand': 'L', 'color': 'green'}
+            ]
+    print(match_gloves(gloves))
+    print(draw_tree(5,"@", 2))
